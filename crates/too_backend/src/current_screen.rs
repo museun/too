9a2cc8df00fdc80e7982d6f44553c0e85c441281
museun <museun@users.noreply.github.com>
2024-09-27 19:23:11 -0400

    document CurrentScreen

--- crates/too_backend/src/current_screen.rs
+++ crates/too_backend/src/current_screen.rs
@@ -1,18 +1,24 @@
+/// Which screen the backend is on
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum CurrentScreen {
+    /// The main screen (the one that doesn't get drawn too)
     Main,
+    /// The alt screen (the one that does get drawn too)
     Alt,
 }
 
 impl CurrentScreen {
+    /// Is this the main screen?
     pub const fn is_main_screen(&self) -> bool {
         matches!(self, Self::Main)
     }
 
+    /// Is this the alt screen?
     pub const fn is_alt_screen(&self) -> bool {
         matches!(self, Self::Alt)
     }
 
+    /// Toggle to the other screen
     pub fn toggle(self) -> Self {
         match self {
             Self::Main => Self::Alt,
